a13a09772c9f9b8b2c0c495c9cd013111c3d3df0
Yeonseo-Jo <jys073190@gmail.com>
2024-01-02 04:18:05 +0900

    chore: 직접 구현 방식 설명 주석 추가

--- src/DragNDrop/components/DndAPI.tsx
+++ src/DragNDrop/components/DndAPI.tsx
@@ -2,20 +2,24 @@ import { useRef, useState } from "react";
 import { DND_ITMES } from "../constants/DND_ITEM";
 
 const DndAPI = () => {
+  //드래그 한 아이템의 인덱스
   const dragItem = useRef<number>();
+  //드랍한 위치에 있는 아이템의 인덱스
   const dragOverItem = useRef<number>();
-
+  //dnd로 변화하는 아이템 값 관리
   const [list, setList] = useState(DND_ITMES);
 
+  //드래그 시작될 때
   const handleDragStart = (idx: number) => {
-    // const position = Number((e.target as HTMLButtonElement).id);
     dragItem.current = idx;
   };
 
+  //드래그 중 대상이 위로 포개졌을 때
   const hanldeDragEnter = (idx: number) => {
     dragOverItem.current = idx;
   };
 
+  //드랍 했을 때 (커서 땠을 때)
   const hanldeDrop = () => {
     const newList = [...list];
     if (!dragItem.current?.toString() || !dragOverItem.current?.toString)
